f54ed2168d5b005b919f5fa884e32fb6440b4913
snowbeta <abkim127@gmail.com>
2018-06-04 22:59:25 -0400

    AK | Objectify pizzabot

--- pizzabot.py
+++ pizzabot.py
@@ -1,59 +1,48 @@
-import re
-import sys
-import argparse
+from lib.point import Point
+from lib.instruction import Instruction
+
+class PizzaBot(Instruction):
+    def __init__(self):
+        super(PizzaBot, self).__init__()
+        self.origin = Point((0, 0))
+
+    def _horizontal_movement(self, movement:list, x_destination):
+        if self.origin.x < x_destination:
+            move = x_destination - self.origin.x
+            movement.append('E'*move)
+        elif self.origin.x > x_destination:
+            move = self.origin.x - x_destination
+            movement.append('W'*move)
+        else:
+            pass
+
+        self.origin.x = x_destination
+
+    def _vertical_movement(self, movement:list, y_destination):
+        if self.origin.y < y_destination:
+            move = y_destination - self.origin.y
+            movement.append('N'*move)
+        elif self.origin.y > y_destination:
+            move = self.origin.y - y_destination
+            movement.append('S'*move)
+        else:
+            pass
+
+        self.y_origin = y_destination
+
+    def deliver_pizza(self):
+        dim = Point(self.zone)
+        drops = map(Point, self.drops)
+        movement = list()
+        for drop in drops:
+            assert drop.x <= dim.x
+            assert drop.y <= dim.y
+            self._horizontal_movement(movement=movement, x_destination=drop.x)
+            self._vertical_movement(movement=movement, y_destination=drop.y)
+            movement.append('D')
+
+        print(''.join(movement))
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("delivery_command", type=str, help="The Dimension of the Delivery")
-args = parser.parse_args()
-
-sep = re.compile(r'[\s\(\)\,]')
-command_ls = sep.split(args.delivery_command)
-command_ls = [val for val in command_ls if val != '']
-
-dimension_val = command_ls[0]
-del command_ls[0]
-dimension_pattern = r'([0-9])x([0-9])'
-dimension_groups = re.match(dimension_pattern, dimension_val)
-
-# Run Time dimension checking
-if dimension_groups:
-    pass
-else:
-    sys.exit("Please provide delivery dimension in '<Horizontal>x<Vertical>'")
-
-horizontal_dimension = int(dimension_groups.group(1))
-vertical_dimension = int(dimension_groups.group(2))
-
-command_ls = list(map(int, command_ls))
-
-origin = [0, 0]
-i = 0
-movement = list()
-for val in command_ls:
-    i += 1
-    if i % 2 == 1:
-        assert val <= horizontal_dimension
-        horizontal_movement = val - origin[0]
-        for v in range(0, abs(horizontal_movement)):
-            if horizontal_movement > 0:
-                movement.append('E')
-            elif horizontal_movement < 0:
-                movement.append('W')
-            else:
-                pass
-        origin[0] = val
-    else:
-        assert val <= vertical_dimension
-        vertical_movement = val - origin[1]
-        for v in range(0, abs(vertical_movement)):
-            if vertical_movement > 0:
-                movement.append('N')
-            elif vertical_movement < 0:
-                movement.append('S')
-            else:
-                pass
-        origin[1] = val
-        movement.append('D')
-
-print(''.join(movement))
+if __name__ == '__main__':
+     PizzaBot().deliver_pizza()
---
+++ pizzabot_script.py
@@ -0,0 +1,49 @@
+import re
+import sys
+import argparse
+from ast import literal_eval
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("delivery_command", type=str, help="The Dimension of the Delivery")
+args = parser.parse_args()
+
+coordinate_pattern = r'(\([0-9]\,\s[0-9]\))'
+command_ls = re.findall(coordinate_pattern, args.delivery_command)
+command_ls = list(map(literal_eval, command_ls))
+
+dimension_pattern = r'([0-9])x([0-9])'
+dimension_groups = re.match(dimension_pattern, args.delivery_command)
+
+# Run Time dimension checking
+if dimension_groups:
+    horizontal_dimension = int(dimension_groups.group(1))
+    vertical_dimension = int(dimension_groups.group(2))
+else:
+    sys.exit("Please provide delivery dimension in '<Horizontal>x<Vertical>'")
+
+# Coordinate Movements
+x_coord, y_coord = (0, 0)
+movement = list()
+for val in command_ls:
+    x_move, y_move = val
+    assert x_move <= horizontal_dimension
+    assert y_move <= vertical_dimension
+    horizontal_movement = x_move - x_coord
+    if horizontal_movement > 0:
+        movement.append('E'*horizontal_movement)
+    elif horizontal_movement < 0:
+        movement.append('W'*abs(horizontal_movement))
+    else:
+        pass
+    vertical_movement = y_move - y_coord
+    if vertical_movement > 0:
+        movement.append('N'*vertical_movement)
+    elif vertical_movement < 0:
+        movement.append('S'*abs(vertical_movement))
+    else:
+        pass
+    movement.append('D')
+    x_coord, y_coord = x_move, y_move
+
+print(''.join(movement))
